15e0db5f32c392787f3e73383a4aeb1c43324bd9
matte156 <matteo.peterlongo@yahoo.com>
2020-12-09 11:58:28 +0100

    Minimax algorithm implementation

--- AI/minumAiFunction.hpp
+++ AI/minumAiFunction.hpp
@@ -7,45 +7,13 @@ using namespace std;
 
 class MiniMax{
     private:
-        int coordinateX[5][2];
-        int coordinateO[4][2];
-        int coordinateCount;
-        // bool checkIfIsFull(int x, int y){
-        //     bool compatible = 0;
-        //     for (int i = 0; i < coordinateCount - 1; i++){
-        //         int coordinateX = coordinateX[i][0];
-        //         int coordinateY = coordinate[i][1];
-        //         if (coordinateX == x && coordinateY == y){
-        //             compatible = 1;
-        //         }
-        //     }
-        //     return compatible;
-        // }
-        void oneParameter(){
-        }
-        void twoParameter(){
-        }
-        void threeParameter(){
-        }
-        void fourParameter(){
-        }
-        void fiveParameter(){
-        }
-        int sixParameter(){
-            return 0;
-        }
-        int sevenParameter(){
-            return 0;
-        }
-        int eightParameter(){
-            return 0;
-        }
+        
     public:
         MiniMax(){
             cout << "Prova!" << endl;
         }
-        MiniMax(int array[2][2], int size){
-            
+        MiniMax(int array[][2], int size){
+
         }
 
         ~MiniMax(){
