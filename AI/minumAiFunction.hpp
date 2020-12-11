3600ab18a0fd5ba883fc42a51014bb4b6424a9c7
matte156 <matteo.peterlongo@yahoo.com>
2020-12-11 12:46:34 +0100

    Minimax implementation

--- AI/minumAiFunction.hpp
+++ AI/minumAiFunction.hpp
@@ -2,18 +2,39 @@
 #define MINIMUMAIFUNCTION_HPP
 
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 class MiniMax{
     private:
-        
+        int** position;
+        int sizePosition;
+        int avaiblePositionArray[0][2];
+        int avaiblePosition(){
+            int coordinateToCheck[] = {1,1};
+            for (int i = 0; i < 9; i++){
+                
+            }
+            return 0;
+        }
     public:
         MiniMax(){
             cout << "Prova!" << endl;
         }
         MiniMax(int array[][2], int size){
-
+            position = new int*[size];
+            for (int i = 0; i < size; ++i){
+                cout << i << endl;
+                position[i] = new int[2];
+            }
+            for (int i = 0; i < size; i++){
+                cout << i << endl;
+                position[i][0] = array[i][0];
+                position[i][1] = array[i][1];
+            }
+            sizePosition = size;
+            avaiblePosition();
         }
 
         ~MiniMax(){
